1d3ff4542f13e05cf9941b3ea6bf9c0be93a0cda
Dmitry.Kononets <d.cononec@gmail.com>
2024-12-27 21:32:32 +0300

    eslint config

--- todo/eslint.config.js
+++ todo/eslint.config.js
@@ -1,27 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import babelEslintParser from '@babel/eslint-parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    plugins: {
-      react: pluginReact,
-      prettier: eslintPluginPrettier,
-      "jsx-a11y": eslintPluginJsxA11y,
-    },
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.es2022,
       },
-      parser: "babel-eslint",
+      parser: babelEslintParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        requireConfigFile: false, // Добавлено, если у вас нет babel.config.js
         ecmaFeatures: {
           jsx: true,
         },
@@ -29,21 +26,20 @@ export default [
     },
   },
   {
-    files: ["**/*.{js,jsx}"],
-    rules: {
-      ...eslintPluginPrettier.rules,
-      "prefer-const": "error",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "on",
-      "prettier/prettier": "error",
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: pluginReact,
+      prettier: eslintPluginPrettier,
+      'jsx-a11y': eslintPluginJsxA11y,
     },
-  },
-  {
-    extends: [
-      "airbnb",
-      pluginJs.configs.recommended,
-      pluginReact.configs.flat.recommended,
-      eslintPluginPrettier.configs.recommended,
-    ],
+    rules: {
+      'prefer-const': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'warn',
+      'prettier/prettier': 'error',
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      ...eslintPluginPrettier.configs.recommended.rules,
+    },
   },
 ];
--- todo/lint-staged.config.js
+++ todo/lint-staged.config.js
@@ -1,18 +1,6 @@
 module.exports = {
-  '*.js': [
-    'eslint --fix',
-    'git add'
-  ],
-  '*.jsx': [
-    'eslint --fix',
-    'git add'
-  ],
-  '*.css': [
-    'stylelint --fix',
-    'git add'
-  ],
-  '*.md': [
-    'prettier --write',
-    'git add'
-  ]
+  '*.js': ['eslint --fix', 'git add'],
+  '*.jsx': ['eslint --fix', 'git add'],
+  '*.css': ['stylelint --fix', 'git add'],
+  '*.md': ['prettier --write', 'git add'],
 };
