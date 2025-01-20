05f12613c48c037d3442d158d9221d706a8a1e7f
Dmitry.Kononets <d.cononec@gmail.com>
2025-01-20 19:42:43 +0300

    Update new-task-form.jsx

--- todo/src/components/new-task-form/new-task-form.jsx
+++ todo/src/components/new-task-form/new-task-form.jsx
@@ -5,8 +5,8 @@ const NewTaskForm = ({ addTask }) => {
   return (
     <form onSubmit={addTask}>
       <input type="text" name="task" className="todo-input task-input" placeholder="What needs to be done?" />
-      <input type="number" name="time-min" className="todo-input time-input" placeholder="Min" />
-      <input type="number" name="time-sec" className="todo-input time-input" placeholder="Sec" />
+      <input type="number" min={0} name="time-min" className="todo-input time-input" placeholder="Min" />
+      <input type="number" min={0} name="time-sec" className="todo-input time-input" placeholder="Sec" />
       <button style={{ display: 'none' }}>Add Task</button>
     </form>
   );
